f58d1be7b8c37389d5f4c167e4cb9d1660d4adfd
CLG\dmoore <devante.moore@cambiumassessment.com>
2022-08-15 10:23:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Try out the function
+        Console.WriteLine(GreatestCommonDenominator(60, 96)); // should be 12
+        Console.WriteLine(GreatestCommonDenominator(20, 8)); // should be 4
+
+        
+    }
+    // Find GCD of two numbers using Euclid's algo
+    public static int GreatestCommonDenominator(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = a;
+            a = b;
+            b = temp % b;
+        }
+        return a;
+    }
+}
+
