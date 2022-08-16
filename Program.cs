fbbe246a5206d131e5fae1fba9c60b83bafbfa5d
CLG\dmoore <devante.moore@cambiumassessment.com>
2022-08-16 11:03:04 -0500

    finished linked list exercise

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        
+        var linkedList = new LinkedList(new Node(1));
+        linkedList.Insert(new Node(3));
+        linkedList.Insert(new Node(6));
+        linkedList.Insert(new Node(10));
+        linkedList.Insert(new Node(17));
+        linkedList.DeleteAt(3);
+        linkedList.GetAll();
     }
 }
 
@@ -11,42 +17,78 @@ public class LinkedList{
     public LinkedList(Node? head=null)
     {
         Head = head;
-        Tail = head;
         _count = head is null ? 0 : 1;
     }
 
     private int _count;
+    private bool _isEmpty => _count == 0;
     public Node? Head { get; set;}
-    public Node? Tail { get; set;}
-    
 
     // methods
+    public void GetAll(){
+        if ( _isEmpty ){
+            Console.WriteLine("No nodes in list");
+            return;
+        }
+        var currentNode = Head;
+        do{
+            Console.Write(currentNode.Value + " -> ");
+            currentNode = currentNode.Next;
+        }while(currentNode is not null);
+        Console.Write("null");
+    }
     public int Count {get{
         return _count;
     }}
     public int? GetNodeData(Node node){
         return node is not null ? node.Value : null;
     }
-    public void Insert(Node data){
-        //TODO
+    public void Insert(Node data){ // O(1)
+        data.Next = Head;
+        Head = data;
+        _count++;
     }
     public Node? Find(int val){
-        //TODO
+        if ( _isEmpty ){
+            Console.WriteLine("No nodes in list");
+            return null;
+        }
+
+        var currentNode = Head;
+        while(currentNode is not null){ // O(n)
+            if (currentNode.Value == val) return currentNode;
+            currentNode = currentNode.Next;
+        }
         return null;
     }
     public Node? DeleteAt(int index){
-        //TODO
-        return null;
+        if ( _isEmpty ) return null;
+        if ( index > Count - 1 ) return null;
+        if ( index == 0 ) {            
+            var tempHead = Head;
+            Console.WriteLine("Deleting " + tempHead + " at index: 0");
+            Head = tempHead.Next;
+            _count--;
+            return tempHead;
+        };
+
+        var currentNode = Head;
+        for(int i = 0; i != index - 1 ; i++){ // stop one short of the given index, fix node that points to the toBeDeleted node
+            currentNode = currentNode.Next;
+        }
+        var nodeToDelete = currentNode.Next;
+        Console.WriteLine("Deleting " + nodeToDelete.Value + " at index: " + index);
+        currentNode.Next = nodeToDelete.Next;
+        _count--;
+        return nodeToDelete;
     }
 }
 public class Node{
-    public Node(int val, Node? previous=null, Node? next=null)
+    public Node(int val, Node? next=null)
     {
-        Previous = previous;
         Next = next;
         Value = val;
     }
-    public Node? Previous { get; set; }
     public Node? Next { get; set; }
     public int Value { get; set; }
 }
